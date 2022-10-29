e1b295ff0748a487d769980080ee90649cb9d05c
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-29 11:30:49 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+let getElement = selector => document.querySelector(selector)
+
+getElement('button[data-start]').addEventListener('click', onColorChangeStart);
+getElement('button[data-stop]').addEventListener('click', onColorChangeStop);
+
+// console.log(getElement('button[data-stop]'));
+
+let intervalID;
+
+function onBackgroundChangeRandom() {
+    getElement('body').style.background = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onColorChangeStart (event) {
+    intervalID = setInterval(onBackgroundChangeRandom, 1000);
+    getElement('button[data-start]').setAttribute('disabled', true)
+};
+
+function onColorChangeStop(event) {
+    clearInterval(intervalID);
+    getElement('button[data-start]').removeAttribute('disabled')
+};
 
