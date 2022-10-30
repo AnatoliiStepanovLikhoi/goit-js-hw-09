2e246de30c9fc46e52b0d32cc69ba8b062be64dc
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-30 16:04:19 +0200

    task 2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,31 @@
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import "flatpickr/dist/themes/material_green.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+Notify.init({
+  width: '350px',
+  position: 'center-top',
+  distance: '10px',
+  opacity: 1,
+  borderRadius: '10px',
+  timeout: 1000,
+})
 
 const options = {
   enableTime: true,
+  enableSeconds: false,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  allowInput: true,
+  dateFormat: 'D, d M Y, H:i',
+  // minDate: Date.now(),
   onClose(selectedDates) {
     // console.log(selectedDates[0]);
-      selectedDate = selectedDates[0];
-      console.log(selectedDate);
-      onInputDataCheck(selectedDate)
+    selectedDate = selectedDates[0];
+    console.log(selectedDate);
+    onInputDataCheck(selectedDate);
+    onTimeSet();
   },
 };
 
@@ -20,20 +33,28 @@ let getElement = selector => document.querySelector(selector)
 let selectedDate = [];
 let intervalID;
 
-console.log(selectedDate);
+const flatpickr = flatpickr(getElement('#datetime-picker'), options);
 
+// console.log(selectedDate);
 // console.log(getElement('#datetime-picker'));
 // console.log(getElement('button[data-start]'));
+// console.log(getElement('button[data-stop]'));
 // console.log(getElement('.value[data-days]'));
 // console.log(getElement('.value[data-hours]'));
 // console.log(getElement('.value[data-minutes]'));
 // console.log(getElement('.value[data-seconds]'));
 
-flatpickr(getElement('#datetime-picker'), options);
-
-getElement('button[data-start]').setAttribute('disabled', true)
-
 getElement('button[data-start]').addEventListener('click', onTimerStart)
+getElement('button[data-stop]').addEventListener('click', onTimerStop)
+
+toggleButtonsActivity()
+// getElement('button[data-start]').setAttribute('disabled', true)
+// getElement('button[data-stop]').setAttribute('disabled', true)
+
+function toggleButtonsActivity() {
+  getElement('button[data-start]').toggleAttribute('disabled');
+  getElement('button[data-stop]').toggleAttribute('disabled');
+};
 
 function onInputDataCheck(date) {
     if (Date.now() > date) {
@@ -42,11 +63,26 @@ function onInputDataCheck(date) {
 
     Notify.success ("Please start the timer!")
     getElement('button[data-start]').removeAttribute('disabled');
-}
+};
 
 function onTimerStart(event) {
-  intervalID = setInterval(() => {
-    let deltaTime = selectedDate - Date.now()
+  intervalID = setInterval(onTimeSet, 1000);
+  // getElement('button[data-stop]').removeAttribute('disabled')
+  // getElement('button[data-start]').setAttribute('disabled', true)
+  // console.log(intervalID);
+  toggleButtonsActivity()
+};
+
+function onTimerStop() {
+  clearInterval(intervalID);
+  onCounterClear();
+  Notify.warning('Timer has stopped');
+  getElement('button[data-stop]').setAttribute('disabled', true)
+  // toggleButtonsActivity()
+};
+
+function onTimeSet() {
+  let deltaTime = selectedDate - Date.now()
 
     if (deltaTime < 0) {
       return
@@ -55,26 +91,28 @@ function onTimerStart(event) {
     let timeData = convertMs(deltaTime);
 
     onInterfaceAdd(timeData)
-    
-    getElement('button[data-start]').setAttribute('disabled', true)
 
     // console.log(timeData);
     // console.log(deltaTime);
-  }, 1000)
-  // console.log(intervalID);
-}
+};
 
 function onInterfaceAdd({ days, hours, minutes, seconds }) {
-
   getElement('.value[data-days]').textContent = days;
   getElement('.value[data-hours]').textContent = hours;
   getElement('.value[data-minutes]').textContent = minutes;
   getElement('.value[data-seconds]').textContent = seconds;
-}
+};
+
+function onCounterClear() {
+  getElement('.value[data-days]').textContent = "00";
+  getElement('.value[data-hours]').textContent ="00";
+  getElement('.value[data-minutes]').textContent = "00";
+  getElement('.value[data-seconds]').textContent = "00";
+};
 
 function addLeadingZero(data) {
   return data.toString().padStart(2, '0');
-}
+};
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -84,7 +122,7 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
@@ -93,7 +131,7 @@ function convertMs(ms) {
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
-}
+};
 
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
