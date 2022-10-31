def0ffeb2b797f00ef15a22cdd3214b9b7e08e5e
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-31 17:26:00 +0000

    Deploying to gh-pages from @ AnatoliiStepanovLikhoi/goit-js-hw-09@1b7902d2594aad7f55b48f8ed816c16b502bff29 🚀

---
+++ 01-color-switcher.9fb778a9.js
@@ -0,0 +1,2 @@
+let t,e=t=>document.querySelector(t);function a(){e("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e("button[data-start]").addEventListener("click",(function(n){t=setInterval(a,1e3),e("button[data-start]").setAttribute("disabled",!0)})),e("button[data-stop]").addEventListener("click",(function(a){clearInterval(t),e("button[data-start]").removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.9fb778a9.js.map
