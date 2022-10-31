92d14883c201cdfc6accb2906c6644b65523c96e
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-31 14:08:33 +0200

    03 elements added

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+let getElement = selector => document.querySelector(selector)
+
+let intervalID = null;
+
+console.log(getElement('.form'));
+console.log(getElement('input [name="delay"]'))
+console.log(getElement('input [name="step"]'))
+console.log(getElement('input [name="amount"]'))
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
